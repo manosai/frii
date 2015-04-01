b914550766b7eeb988a4b5bd9039761102627ae4
Manosai <mano.eerabathini@gmail.com>
2015-04-01 14:32:21 -0400

    initial commit

---
+++ Frii/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Frii
+//
+//  Created by Manosai Eerabathini on 4/1/15.
+//  Copyright (c) 2015 Frii, Inc. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
